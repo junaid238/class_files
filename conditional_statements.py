83b5e5a5473a78ff623767ecca8482d952dbe537
Khan <mkhan@digitallynctech.om>
2018-04-02 12:25:13 +0530

    30 march class files

---
+++ conditional_statements.py
@@ -0,0 +1,76 @@
+# conditional statements 
+# ----------------------
+# exec stops at wrong stmt 
+# if else 
+
+# condition -->  t / f
+# 	if T --> stmt1
+# 	else F --> may or maynot stmt2 
+
+# condition1
+# 	if T --> stmt1
+
+# 	elif F --> condition2 
+# 				else --> T --> stmt2
+# 				--> F --> stmt3
+
+# if elif else 
+# if elif elif elif else 
+
+# nested condition statements
+# ---------------------------
+# if 
+# 	if 
+# 		if 
+# 		else
+# 	else
+# else
+
+
+# Syntax:
+# -------
+# C CPP Java
+# if(condition){
+# tstmt
+# }
+# else{
+# 	fstmt
+# }
+
+
+# Python
+# ------ 
+# if condition :
+# 	tstmt
+
+# else:
+# 	fstmt
+
+# if (condition) :
+# 	tstmt
+
+# else:
+# 	fstmt
+
+
+
+
+# a = 11 
+# if a==10:
+# 	print("right")
+# elif a==11:
+# 	print("hai")
+# else:
+# 	print("wrong")
+
+
+# b = 101 
+# if (b==10):
+# 	pass
+# else:
+# 	print("wrong")
+
+
+
+
+
