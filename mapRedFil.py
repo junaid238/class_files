929eff5bd925badc2b751ea855334561c8452dbe
Khan <mkhan@digitallynctech.om>
2018-05-23 10:27:25 +0530

    added map reduce filter

---
+++ mapRedFil.py
@@ -0,0 +1,34 @@
+# LAMBDA 
+# incnum = lambda x:x+1
+# print(incnum(10))
+# print(incnum(20))
+
+# squaring = lambda x :x**2
+# print(squaring(20))
+
+# addnums = lambda x,y : x+y
+# print(addnums(10,11))
+
+# MAP 
+# l1 = [1,2,3,4,5,8 ]
+# l2 = [10,11,12,13,14,15]
+
+# sqa = map(lambda x :x**2 , l1)
+# sqa = list(map(lambda x :x**2 , l1))
+# print(sqa)
+
+# addl = list(map(lambda x, y : x+y  , l1 ,l2))
+# print(addl)
+
+
+# FILTER 
+# fil = list(filter(lambda x : not x%3==0  , l2))
+# print(fil)
+
+#REDUCE
+# from functools import reduce
+# red = reduce(lambda x ,y : x*y  , l1)
+# print(red)
+
+
+
